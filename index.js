f3ded0f29367371ee168a2485f77673542442595
cravsky <cravsky@gmail.com>
2024-09-02 09:32:57 +0200

    Implemented rate limiting and caching

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 5 // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+app.set('trust proxy', 1); // trust first proxy
 
 // Routes
 app.use('/api', require('./routes/index'));
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,48 @@
 const express = require('express');
 const router = express.Router();
 const needle = require('needle');
+const url = require('url');
+const apicache = require('apicache');
+
 
 // Env vars
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
+// Cache
+let cache = apicache.middleware;
 
-router.get('/', async (req, res) => {
+
+router.get('/', cache('2 minutes'), async (req, res) => {
+
+    // TODO: Replace needle with axios
 
     try {
-        // const apiRes = await needle('get', `${API_BASE_URL}`);
-        const apiRes = await needle('get', `${API_BASE_URL}/popular?api_key=${API_KEY_VALUE}&language=en-US&page=1`);
+        const params = new URLSearchParams({
+            // More universal approach
+            // ...url.parse(req.url, true).query
+            [API_KEY_NAME]: API_KEY_VALUE,
+            language: 'en-US',
+            page: 1,
+        });
+        // popular - parameter from frontend. How to get it?
+        // I could split it into 3 requests
+
+        const apiRes = await needle('get', `${API_BASE_URL}/popular?${params}`);
 
         const data = apiRes.body;
+
+        if (process.env.NODE_ENV !== 'production') {
+            console.log(`REQUEST: ${API_BASE_URL}/popular?${params}`);
+        }
         res.status(200).json(data);
     } catch (error) {
         res.status(500).json({
             message: error.message,
             success: false
         });
-        
+
     }
 
 
